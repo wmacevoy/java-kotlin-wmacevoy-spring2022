9df90b09607eb72f72fad7417f0e7b372dc32fc5
Warren MacEvoy <wmacevoy@gmail.com>
2022-03-29 12:17:14 -0600

    stream

--- Streams/app/src/main/java/com/example/streams/Path.java
+++ Streams/app/src/main/java/com/example/streams/Path.java
@@ -1,6 +1,11 @@
 package com.example.streams;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 public class Path extends ArrayList<Dot> {
     double lengthInKm() {
@@ -9,5 +14,21 @@ public class Path extends ArrayList<Dot> {
             ans += get(i).distanceToInKm(get(i + 1));
         }
         return ans;
+    }
+
+    double distanceToInKm(Dot to) {
+        double minDist = Double.MAX_VALUE;
+        for (int i = 0; i < size(); ++i) {
+            minDist = Math.min(minDist, get(i).distanceToInKm(to));
+        }
+        return minDist;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    double distanceToInKmInParallel(Dot to) {
+        return parallelStream()
+                .mapToDouble(dot -> dot.distanceToInKm(to))
+                .min()
+                .orElse(Double.MAX_VALUE);
     }
 }
